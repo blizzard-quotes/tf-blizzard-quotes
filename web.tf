ede7be0f3f935f7d28106e8349ecea5c09d37162
Whitebread <williampanebianco@gmail.com>
2020-12-16 22:49:25 -0500

    Add www domain

--- web.tf
+++ web.tf
@@ -2,6 +2,7 @@ module "blizzard_quotes_ui" {
   source = "github.com/whitebread-cloud/terraform-aws-s3-cloud-front-route-53"
 
   hostname            = local.blizzard_quotes_hostname
+  alt_hostnames       = ["www.${local.blizzard_quotes_hostname}"]
   route_53_zone_name  = local.blizzard_quotes_hostname
   acm_certificate_arn = aws_acm_certificate.blizzard_quotes.arn
   s3_web_bucket       = local.s3_bucket_blizzard_quotes_ui_name
